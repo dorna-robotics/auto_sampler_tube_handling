257e32cc34239671c35129601c83ffdaaf2f4f80
Andre Mixan <amixan@umich.edu>
2025-10-13 19:08:16 -0700

    Added sorted vision-guided vial picking

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ robot = {
 # Motion control and sim
 #############################################################
 sim = 0
-speed = 1
+speed = 0.3
 sleep = 0
 num = 25
 
@@ -47,6 +47,19 @@ robot_gripper = {
     "cap_decap": [0, 0, 12+27+6.9, 0, 0, 0], #Z=screw out of gripper+gripper+gripper base, this gives the lowest point of the screws
     "open": [[1, 0, 0.25]],
     "close": [[1, 1, 0.25]],
+    "vial":{
+        "tool": [0, 0, 29, 0, 0, 0],
+        "close": [[0, 1, 0.25]],
+        "open": [[0, 0, 0.25]],
+        "finger_width": 10,
+        "gripper_opening": 43,
+        "finger_location": [0, 180],
+        "rvec_base": [180, 0, 0],
+        "gripper_rotation": [
+            {"axis": [0, 0, 1], "angle": 0},
+            {"axis": [0, 0, 1], "angle": 180},
+        ]
+    }
 }
 
 #############################################################
@@ -68,7 +81,8 @@ vial_holder = {
     "aux": [485, 0],
     "frame": [31*25+12.5-(1.4+12.5)/2, -(10*25+12.5)+(1.4+12.5)/2, 0, 180, 0, 0], #Position of vial at col 3, row 4 from tray's bottom left corner
     "pick": [-0.05716812026093976, 1.1853255529723015, -33.06382287695952, 0.23287556085751854, -0.3172811067209202, 47.98701517310126],
-    "drop": [-0.1455158211095977, 1.2360057567397007, -37.077112148668476, 0.2327585229236095, -0.3173253109692344, 48.00894415631942]
+    "drop": [-0.1455158211095977, 1.2360057567397007, -37.077112148668476, 0.2327585229236095, -0.3173253109692344, 48.00894415631942],
+    "grid_spacing": 12.5 + 1.4
 }
 
 cap_holder = {
@@ -76,6 +90,37 @@ cap_holder = {
     "frame": [37*25+12.5, -(6*25+12.5), 0, 180, 0, 0],
     "pick": [0.5286622981476512, 3.5193017123831964, -10.858378489849372, -4.769162562411459e-16, 2.572475798769063e-15, -42.011718],
     "drop": [-3.1190497933401957, 3.1627060997030867, -13.228152233190258, 0.02347517880592545, -0.08709918072320023, 45.15379995820485] #Done before robot recalibration, no longer valid
+}
+
+camera = {
+    "aux": [485, 0],
+    "frame": [31*25+12.5, -(10*25+12.5), 0, 180, 0, 0],
+    "vials": [-0.8261485518240761, -52.573060583650374, -84.59651760709431, 0.020111097002960723, -0.00885000707069195, 1.5043170460124506e-13], #Calculated from "frame" and "joints" using gripper["cap_decap"]
+    # "joints": [-66.247559, 6.547852, -109.248047, 0, 12.678223, -66.247559] # Original
+    # "joints": [-66.379395, 5.712891, -112.346191, 0.021973, 16.611328, -66.379395] # Previous
+    "joints": [-64.907227, 6.569824, -109.138184, 0.021973, 12.546387, -64.929199],
+    "ind_vial": [4.215628633946608, -55.61708449389786, -84.58102105223001, 0.017875335293693777, -0.013641431814762501, -0.0005246204567250441],
+    "z_guess": 141.5
+}
+
+#############################################################
+# detections
+#############################################################
+detection_preset = {
+    "tube":{
+        "camera_mount":{
+            "type": "dorna_ta_j4",
+            "ej": [0 ,0, 0, 0, 0, 0, 0, 0],
+            "T": [46.5174596+1+1+0+4-(5+0.75), 32.0776662-3+1-0-1.5+(-3+2.2), -4.24772615-3, -0.27547989, 0.27691881, 89.6939516],
+        },
+        "detection": {"cmd": "od", "path": "model/tube.pkl", "conf": 0.2, "cls": []},
+        #'roi': {'corners': [[214.51, 11.63], [220.24, 377.26], [611.69, 374.4], [613.13, 10.19]], 'inv': False, 'crop': True, 'offset': 0},
+        "roi": {"corners": [[652.99, 428.85], [675.93, 7.29], [174.08, 2.98], [179.81, 437.45]], "inv": False, "crop": True, "offset": 0},
+        "sort": {"cmd": "shuffle", "max_det": 100}, 
+        #'limit': {'xyz': {'x': [-150, 110], 'y': [200, 450], 'z': [110, 130], 'inv': 0}}, 
+        "display": {"label": 0, "save_img": False, "save_img_roi": True}
+        },
+    "limit": {"xyz": {"x": [-150, 110], "y": [200, 450], "z": [110, 130], "inv": 0}}, 
 }
 
 #############################################################
@@ -92,6 +137,32 @@ safe_init = {
     "sleep": sleep, 
     "motion": "lmove", "speed": speed, "cont": 0, "corner": 250, 
     "freedom": {"num":num, "range":[0.05, 0.05, 0.05], "early_exit":False}, "timeout": -1, "sim":sim
+}
+
+take_init_picture = {
+    "pick": {
+        "type": "joint", # robot, world, joint
+        "loc": [camera["joints"], camera["aux"]]
+    },
+    "base_in_world": robot["base_in_world"],
+    "aux_dir": robot["aux_dir"],
+    "sleep": sleep, 
+    "motion": "jmove", "speed": speed, "cont": 0, "corner": 250, 
+    "freedom": {"num":num, "range":[0.05, 0.05, 0.05], "early_exit":False}, "timeout": -1, "sim":0
+}
+
+take_ind_picture = {
+    "pick": {
+        "type": "world", # robot, world, joint
+        "loc": [camera["ind_vial"], camera["aux"]],
+        "frame": camera["frame"],
+        "tool": [robot_gripper["cap_decap"], robot_gripper["cap_decap"]], #[0]: Tool defiinition before grabbing, [1]: Tool after grabbing
+    },
+    "base_in_world": robot["base_in_world"],
+    "aux_dir": robot["aux_dir"],
+    "sleep": sleep, 
+    "motion": "jmove", "speed": speed, "cont": 0, "corner": 250, 
+    "freedom": {"num":num, "range":[0.05, 0.05, 0.05], "early_exit":False}, "timeout": -1, "sim":0
 }
 
 vial_tray_to_decapper = {
@@ -117,7 +188,7 @@ vial_tray_to_decapper = {
     "base_in_world": robot["base_in_world"],
     "aux_dir": robot["aux_dir"],
     "sleep": sleep, 
-    "motion": "lmove", "speed": speed, "cont": 0, "corner": 250, 
+    "motion": "jmove", "speed": speed, "cont": 0, "corner": 250, 
     "freedom": {"num":num, "range":[0.05, 0.05, 0.05], "early_exit":False}, "timeout": -1, "sim":sim
 }
 
@@ -160,7 +231,7 @@ cap_to_vial = {
         "loc": [decapper["cap_drop"], decapper["aux"]],
         "frame": decapper["frame"],
         "tool": [robot_gripper["cap_decap"], robot_gripper["cap_decap"]],
-        # "approach": [[0, 0, -24, 0, 0, 0]]
+        "approach": [[0, 0, -20, 0, 0, 0]]
     },
     "base_in_world": robot["base_in_world"],
     "aux_dir": robot["aux_dir"],
--- tools.py
+++ tools.py
@@ -2,64 +2,127 @@
 import necessary modules
 """
 from dorna2 import pose as dorna_pose
+from dorna_vision import Detection
+from dorna_vision import grasp
+from dorna_vision import Valid
 import home
 import home_config
 import numpy as np
-
-def start_event(msg, union, dorna_robot):
-    if "in1" in msg and msg["in1"] == 1 and (dorna_robot.prm_stop or dorna_robot.prm_pause):
-        dorna_robot.set_alarm(0)
-        dorna_robot.prm_stop = False
-        dorna_robot.prm_pause = False
-        dorna_robot.prm_enable = True
-        if dorna_robot.completed_sequence:
-            dorna_robot.completed_sequence = False
-            print("Starting new sequence")
-        print("Start pressed, prm_stop: ", dorna_robot.prm_stop, " prm_pause: ", dorna_robot.prm_pause)
-
-def stop_event(msg, union, dorna_robot):
-    if "in2" in msg and msg["in2"] == 0 and not dorna_robot.prm_stop:
-        dorna_robot.prm_stop = True
-        dorna_robot.halt()
-        dorna_robot.set_alarm(1)
-        dorna_robot.prm_enable = False
-        dorna_robot.completed_sequence = False
-        if dorna_robot.sequence_started:
-            dorna_robot.sequence_cancelled = True
-        print("Stop pressed, prm_stop: ", dorna_robot.prm_stop)
-
-def pause_event(msg, union, dorna_robot):
-    if "in0" in msg and msg["in0"] == 0 and not dorna_robot.prm_pause:
-        dorna_robot.prm_pause = True
-        print("Pause pressed, prm_pause: ", dorna_robot.prm_pause)
-
-def alarm_event(msg, union, dorna_robot):
-    if "alarm" in msg and msg["alarm"] == 1 and not dorna_robot.prm_stop:
-        dorna_robot.prm_stop = True
-        dorna_robot.halt()
-        dorna_robot.prm_enable = False
-        dorna_robot.completed_sequence = False
-        if dorna_robot.sequence_started:
-            dorna_robot.sequence_cancelled = True
-        print("Alarm activated, prm_stop: ", dorna_robot.prm_stop)
+import json
+import time
 
 def update_robot_frame(config):
     config.robot["robot_x_world_off"] = config.robot["rail_screw_pos"] + config.robot["rail_screw_diameter"]/2 - config.robot["rail_scew_dist"] + config.robot["robot_base_x"] + config.robot["rail_x_ref"]
     config.robot["base_in_world"][0] = config.robot["robot_x_world_off"]
 
-def update_vial_frame(config, vial_frame=[0,0], rail_x=485):
-    config.vial_holder["frame"][0] = vial_frame[0]
-    config.vial_holder["frame"][1] = vial_frame[1]
+def update_camera_frame(config, camera_frame):
+    # config.camera["frame"][0] = camera_frame[0] + config.robot["base_in_world"][0] + config.camera["aux"][0]
+    # config.camera["frame"][1] = camera_frame[1]
+    config.camera["frame"][:2] = camera_frame[:2]
+    print("Camera in world position updated")
+
+# def update_vial_frame(config, vial_frame, rail_x=485):
+#     config.vial_holder["frame"][0] = vial_frame[0]
+#     config.vial_holder["frame"][1] = vial_frame[1]
+#     config.vial_holder["aux"][0] = rail_x
+
+#     print("Vial in world position updated")
+
+def update_vial_frame_vision(config, vial_frame_vision, rail_x=485):
+    # config.vial_holder["frame"][0] = vial_frame_vision[0] + config.robot["base_in_world"][0] + config.camera["aux"][0]
+    # config.vial_holder["frame"][1] = vial_frame_vision[1]
+    config.vial_holder["frame"][:2] = vial_frame_vision[:2]
     config.vial_holder["aux"][0] = rail_x
 
+    # print("Vial frame: ", config.vial_holder["frame"], "Vial aux: ", config.vial_holder["aux"])
     print("Vial in world position updated")
 
-def update_cap_frame(config, cap_frame=[0,0], rail_x=485):
+def update_cap_frame(config, cap_frame, rail_x=485):
     config.cap_holder["frame"][0] = cap_frame[0]
     config.cap_holder["frame"][1] = cap_frame[1]
     config.cap_holder["aux"][0] = rail_x
 
     print("Cap in world position updated")
+
+def correct_vision_xyz(config, vision_xyz, pxl, camera, camera_data, detection):
+    z_guess = config.camera["z_guess"]
+    z_gt = (z_guess, z_guess)
+    xyz_target_to_cam = camera.xyz(pxl=pxl, depth_frame=camera_data["depth_frame"], depth_int=camera_data["depth_int"], z_gt=z_gt)
+
+    T_target_to_cam = detection.kinematic.xyzabc_to_mat(np.concatenate((np.array(xyz_target_to_cam[0]), np.array([0, 0, 0]))))
+
+    # apply frame
+    T_target_to_frame = np.matmul(detection.frame_mat_inv, T_target_to_cam)
+    xyz_target_to_frame = detection.kinematic.mat_to_xyzabc(T_target_to_frame).tolist()
+    corrected_xyz = xyz_target_to_frame[0:3]
+    return corrected_xyz
+
+def translate_vis_to_world(config, xyz): #Naive translation, currently only corrects X as Y is already correct and this Z is disregarded by the pick_and_place motions
+    corrected_xyz = xyz
+    corrected_xyz[0] += config.robot["base_in_world"][0] + config.camera["aux"][0]
+    return corrected_xyz
+
+def cluster_coordinates(values, spacing, tolerance=0.6):
+    """Cluster coordinates into grid positions"""
+    sorted_vals = sorted(values)
+    clusters = []
+    current_cluster = [sorted_vals[0]]
+    
+    for val in sorted_vals[1:]:
+        if val - current_cluster[-1] < spacing * tolerance:
+            current_cluster.append(val)
+        else:
+            clusters.append(current_cluster)
+            current_cluster = [val]
+    clusters.append(current_cluster)
+    
+    # Return average of each cluster
+    return [sum(c) / len(c) for c in clusters]
+
+def assign_to_grid(point, row_centers, column_centers):
+    x, y, z = point[0], point[1], point[2]
+    
+    # Find nearest row (based on X) and column (based on Y)
+    row_idx = min(range(len(row_centers)), 
+                key=lambda i: abs(row_centers[i] - x))
+    col_idx = min(range(len(column_centers)), 
+                key=lambda i: abs(column_centers[i] - y))
+    
+    return (row_idx, col_idx, point)
+
+def sort_vials(config, points):
+    grid_spacing = config.vial_holder["grid_spacing"]
+    # Extract x and y coordinates
+    x_coords = [p[0] for p in points]  # More positive = bottom, more negative = top
+    y_coords = [p[1] for p in points]  # More positive = left, more negative = right
+
+    # Find grid columns and rows (reference positions)
+    # Sort x in reverse (top to bottom: most positive last)
+    # Sort y in reverse (left to right: most positive first)
+    column_centers = cluster_coordinates(y_coords, grid_spacing)  # Y defines columns (left-right)
+    row_centers = cluster_coordinates(x_coords, grid_spacing)     # X defines rows (top-bottom)
+
+    # Reverse row_centers so index 0 = top (most negative X)
+    row_centers.reverse()
+    # Reverse column_centers so index 0 = left (most positive Y)
+    column_centers.reverse()
+
+    print(f"Found {len(row_centers)} rows")
+    print(f"Found {len(column_centers)} columns")
+
+    # Assign all points to grid
+    grid_points = [assign_to_grid(p, row_centers, column_centers) for p in points]
+
+    # Sort by row (top to bottom), then by column (left to right)
+    sorted_grid_points = sorted(grid_points, key=lambda x: (x[0], x[1]))
+
+    # Extract just the points in the correct order
+    sorted_points = [p[2] for p in sorted_grid_points]
+
+    # print("\nSorted points (left to right, top to bottom):")
+    # for i, point in enumerate(sorted_points, 1):
+    #     print(f"{i}. {point}")
+    return sorted_points
 
 def output(config, robot, timeout=-1): 
     cmd_list = []   
@@ -119,6 +182,14 @@ def init(config, robot, calibrate=False):
 
 def safe_init(config, robot):
     return robot.pick_n_place(**config.safe_init)
+
+def take_init_picture(config, robot):
+    joints = config.camera["joints"] + config.camera["aux"]
+    robot.go(joint=joints, motion="jmove", freedom=config.take_init_picture["freedom"], cont=0, timeout=-1)
+    # return robot.pick_n_place(**config.take_init_picture)
+
+def take_ind_picture(config, robot):
+    return robot.pick_n_place(**config.take_ind_picture)
 
 def vial_tray_to_decapper(config, robot):
     return robot.pick_n_place(**config.vial_tray_to_decapper)
@@ -243,6 +314,67 @@ def capping(config, robot, pose_init=None):
 
 def vial_to_tray(config, robot):
     return robot.pick_n_place(**config.vial_to_tray)
+
+def vial_tray_to_decapper_vision(config, robot, detection):
+    retval = None
+
+    # imaging joint
+    robot.go(joint=config.camera["joints"]+ config.tube_pick["aux"], speed=config.speed, sim=0)
+    time.sleep(0.5)
+
+    # run detection
+    result_tube = detection.run()
+    for r in result_tube:
+        # not a cap
+        if r["cls"] not in config.robot_gripper:
+            continue
+        
+        # valid xyz
+        if not Valid().xyz(r["xyz"], **config.detection_preset["limit"]["xyz"]):
+            continue
+
+        # best_pick
+        best_rvec_pick = grasp.collision_free_rvec(
+            r["id"], 
+            config.two_finger_gripper[r["cls"]]["rvec_base"], 
+            config.two_finger_gripper[r["cls"]]["gripper_opening"],
+            config.two_finger_gripper[r["cls"]]["finger_width"],
+            config.two_finger_gripper[r["cls"]]["finger_location"], 
+            detection, mask_type="elp", prune_factor=4, num_steps=360)        
+        if best_rvec_pick is None:
+            print("no grasp found")
+            continue
+        
+        # all the possible solutions
+        pose_all = [r["xyz"] + dorna_pose.rotate_abc(best_rvec_pick, axis=rotation["axis"], angle=rotation["angle"], local=True) for rotation in config.two_finger_gripper[r["cls"]]["gripper_rotation"]]
+
+        # nearest pose
+        best_pose = robot.kinematic.nearest_pose(pose_all, detection.retval["camera_data"]["joint"], config.rack_to_scanner["freedom"])
+        if best_pose is None:
+            print("no nearest pose found")
+            continue
+        
+        # adjust pick_n_place
+        config.rack_to_scanner["pick"]["loc"][0] = best_pose
+        config.rack_to_scanner["pick"]["tool"] = 2*[config.two_finger_gripper[r["cls"]]["tool"]]
+        config.rack_to_scanner["pick"]["output"] = config.two_finger_gripper[r["cls"]]["close"]
+        config.rack_to_scanner["place"]["tool"] = 2*[config.two_finger_gripper[r["cls"]]["tool"]]
+        
+        # rack_to_scanner
+        result = robot.pick_n_place(**config.rack_to_scanner)
+        if not result:
+            print("pick and place failed")
+            continue
+        try:
+            for res in result:
+                print(json.dumps(res))
+        except:
+            pass
+        
+        retval = r["cls"], best_pose
+        break
+    
+    return retval
 
 def generate_vials_caps_coords():
     # center_vial = [31*25+12.5, -(10*25+12.5)] #These are frame coordinates, calculated based on position on base plate
